fb5c590f9d83d5ee9dacd5e604cd933b1e81471c
Matthew Bell <matthewbell1030@gmail.com>
2020-05-11 16:48:06 -0700

    re-attempting to commit 'db.js'.  The latest version of this file had not been pushed.  I also added a comment in index.html.

--- public/db.js
+++ public/db.js
@@ -1,8 +1,64 @@
-// create the database
+// CREATE DB
 let db;
 
 // create a new db request
 const request = indexedDB.open("budget", 1);
 
+// create object store
+request.onupgradeneeded = (event) => {
+    const db = event.target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = ({ target }) => {
+    db = target.result;
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+// log errors here
+request.onerror = (event) => {
+    console.log(event);
+};
+
+// create transaction on pending db, and add record to obectStore
+const saveRecord = (record) => {
+    console.log(record);
+    const transaction = db.transaction("pending", "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
+}
+
+// get all records from store
+const checkDatabase = () => {
+    const transaction = db.transaction("pending", "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "appplication/json",
+                },
+            })
+            .then((response) => response.json())
+            // delete records
+            .then(() => {
+                const transaction = db.transaction("pending", "readwrite");
+                const store = transaction.objectStore("pending");
+                store.clear();
+            })
+        }
+    }
+}
+
+
+
 // EVENT LISTENERS
 // listener for when app comes back online
+window.addEventListener("online", checkDatabase)
--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://StarMoose42:<password>@cluster0-3pwbk.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -17,7 +26,8 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  
 });
 
 // routes
